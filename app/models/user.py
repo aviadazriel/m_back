9ea04f05bd021271c04ebeac05514c8646ff1a3b
Aviad Azriel <aviadazriel2@gmail.com>
2025-01-03 11:16:26 +0200

    edit user model

--- app/models/user.py
+++ app/models/user.py
@@ -2,20 +2,27 @@ from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, text
 from app.database import Base
 from sqlalchemy import Column, Integer, String, Text, Date, ARRAY, TIMESTAMP
 
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: str = None
+
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, nullable=False)
     email = Column(String(100), unique=True, nullable=False)
-    phone = Column(String(10), unique=True, nullable=False)
-    password_hash = Column(String(255), nullable=False)
+    phone = Column(String(10), unique=True, nullable=True)
+    password = Column(String(255), nullable=False)
     first_name = Column(String(50))
     last_name = Column(String(50))
     profile_image_url = Column(String)
-    is_active = Column(Boolean, default=True)
-    is_admin = Column(Boolean, default=False)
     created_at = Column(TIMESTAMP, server_default=text("CURRENT_TIMESTAMP"))
     updated_at = Column(TIMESTAMP, server_default=text("CURRENT_TIMESTAMP"), onupdate=text("CURRENT_TIMESTAMP"))
 
